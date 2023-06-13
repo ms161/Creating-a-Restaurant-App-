d0c689b2d39a8ffdd43f06c730fbb31a3843302b
ms161 <msehrawat161>
2023-06-13 06:02:26 +0530

    Making a hard Coded Cart

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import React, { useState } from 'react';
+import Cart from './Components/Cart/Cart';
 import Header from './Components/Layout/Header';
 import Meals from './Components/Meals/Meals';
 
@@ -6,6 +7,7 @@ import Meals from './Components/Meals/Meals';
 const App = () => {
  return (
   <>
+  <Cart/>
 <Header/>
   <main>
 <Meals/>
--- src/Components/Meals/AvailableMeals.jsx
+++ src/Components/Meals/AvailableMeals.jsx
@@ -32,6 +32,7 @@ const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price} />
   ));
+  console.log(mealsList)
   return (
     <>
       <section className={classes.meals}>
